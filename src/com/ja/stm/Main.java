305d65d835c4d90ec0000ee585d27a4f3b08c5ac
minjizzangminjichoigo <dlalswl3513@naver.com>
2023-09-19 16:21:12 +0900

    123154

--- src/com/ja/stm/Main.java
+++ src/com/ja/stm/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("안녕");
-		System.out.println("선착순 1"); 
+		System.out.println("선착순 1");
+		System.out.println("선착순 2");
+		System.out.println("선착순 3");
 	}
 
 }
